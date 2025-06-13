1f3721a49434ec16e4a894b396aed08f5fc726d9
Barna I. Iosif <barnaiosif20@gmail.com>
2025-06-13 11:37:33 +0000

    First commit

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'R1rGzyLdUidIrCjtoSSPgU0SKpXN33YVv9nozWqVMQrawVPoSDEyApDcrrwgR34N');
+define('SECURE_AUTH_KEY',  'SuYgxq9Hcq1LdKMx5XUznJ165mPYwoHwtHMBcK4IfdRhnsJm0wSBdYDNig26rd6W');
+define('LOGGED_IN_KEY',    'NJMu61WY6DUKLjn1FzXsgeDYo3a1t6gc1YI8yebTg6nXKVRrwr7ad8ut3JrfKqTj');
+define('NONCE_KEY',        'PGNMvp6WxDNWJYmg1codpd9CSGduT1RM47fK3dqNCu6yNwWCG8DPhECvcsgpD1H4');
+define('AUTH_SALT',        '5INK1H4WQ0Ng9y90PfyidAGE6dhtLdiqNN8QtynqCcI020TXC43CBb4dQ0UUcQ8N');
+define('SECURE_AUTH_SALT', '4EdHQ5ic6C9cQy5pcPzentaUvwrWA4yUK6nnHp3dDuNCs7rWR3gg8oUf2UNcJFCp');
+define('LOGGED_IN_SALT',   'MpYRbwbIaImN4qngAFeqgW0V1SbVdVHwBoRrab5KiE7TVW4BVGK3a9EYVSMptgji');
+define('NONCE_SALT',       'GGymxFYhUWeETH5eaDdBhQMCgi2u9snS7GvmNhPg716Ey6c2UhyVGri465SGC8Yx');
